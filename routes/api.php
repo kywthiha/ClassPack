6680f4dfcdc1ebe3cdbb6127636600fd87434294
kyawthiha <kyawthiha.dev@gmail.com>
2022-04-16 20:38:17 +0630

    feat:Authentication with passport

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+
+Route::group(['prefix' => 'auth'], function () {
+
+    Route::post('register', [UserController::class, 'register'])->name('register');
+    Route::post('login', [UserController::class, 'login'])->name('login');
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [UserController::class, 'logout'])->name('logout');
+        Route::get('profile', [UserController::class,'profile'])->name('profile');
+    });
+
+});
