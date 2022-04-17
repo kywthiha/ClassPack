449445f21cb627d3de91884415ef58198217ac14
kyawthiha <kyawthiha.dev@gmail.com>
2022-04-18 00:13:50 +0630

    feat:activity store with repository

--- app/Observers/ActivityLogObserver.php
+++ app/Observers/ActivityLogObserver.php
@@ -2,12 +2,24 @@
 
 namespace App\Observers;
 
+use App\Interfaces\ActivityLogRepositoryInterface;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 
 class ActivityLogObserver
 {
     public $afterCommit = true;
+
+
+    private ActivityLogRepositoryInterface $activityLogRepository;
+
+    public function __construct(ActivityLogRepositoryInterface $activityLogRepository)
+    {
+        $this->activityLogRepository = $activityLogRepository;
+    }
+
+
+
 
     private function storeLog(Model $model)
     {
@@ -25,17 +37,13 @@ class ActivityLogObserver
             'request_query' => request()->query(),
             'request_server' => request()->server()
         ];
+
         try {
             Log::info('ActivityLogObserver: ' . $model->getTable() . '_' . $model->getKey() . '_' . debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 2)[1]['function'], $logData);
         } catch (\Exception $e) {
             Log::error('ActivityLogObserver: ' . $e->getMessage());
         }
-
-        try {
-            \App\Models\ActivityLog::create($logData);
-        } catch (\Exception $e) {
-            Log::error('ActivityLogObserver: ' . $e->getMessage());
-        }
+        $this->activityLogRepository->storeLog($model, $logData);
     }
 
     public function created(Model $model)
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use App\Interfaces\ActivityLogRepositoryInterface;
 use App\Interfaces\OrderRepositoryInterface;
 use App\Interfaces\PackRepositoryInterface;
 use App\Interfaces\PromoCodeRepositoryInterface;
 use App\Interfaces\UserRepositoryInterface;
+use App\Repositories\ActivityLogRepository;
 use App\Repositories\OrderRepository;
 use App\Repositories\PackRepository;
 use App\Repositories\PromoCodeRepository;
@@ -28,6 +30,8 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(PromoCodeRepositoryInterface::class, PromoCodeRepository::class);
 
         $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
+
+        $this->app->bind(ActivityLogRepositoryInterface::class, ActivityLogRepository::class);
     }
 
     /**
